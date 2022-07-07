ced27f46ff2814c7f06193eb3b91d1be7e75f9f5
s20020 <s20020@std.it-college.ac.jp>
2022-07-07 14:55:14 +0900

    Initial commit

---
+++ app/src/main/java/com/example/firebase001/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.firebase001
+
+import android.graphics.BitmapFactory
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
