d74a99738536380f070510d9b5747748d3da3aca
s20020 <s20020@std.it-college.ac.jp>
2022-07-07 16:54:12 +0900

    なんとなく設定終わった感じ？

--- app/src/main/java/com/example/firebase001/MainActivity.kt
+++ app/src/main/java/com/example/firebase001/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.firebase001
 
+import android.content.Context
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.util.Log
+import android.widget.Toast
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
